c52683e715fc0f0e635dcf8c2bec8c8c4f597e88
RomanCizmarik <roman.cizmarik@gmail.com>
2017-11-21 17:25:01 +0100

    Moved implementation to MainWindow and CSene

---
+++ include/CScene.h
@@ -0,0 +1,31 @@
+#ifndef CSCENE_H
+#define CSCENE_H
+
+#include<QObject>
+
+//forward declaration
+namespace Qt3DCore
+{
+    class QEntity;
+};
+
+class CScene : QObject
+{
+    Q_OBJECT
+
+public:
+
+    CScene();
+    ~CScene();
+
+    Qt3DCore::QEntity *getRootEntity() { return m_rootEntity; }
+    void setRootEntity(Qt3DCore::QEntity *e) { m_rootEntity = e; }
+
+    //TODO: Test method - delete later
+    void createSphere();
+
+private:
+    Qt3DCore::QEntity *m_rootEntity;
+};
+
+#endif
